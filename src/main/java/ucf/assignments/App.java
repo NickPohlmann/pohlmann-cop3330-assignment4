387b9641f180e2577632ff16d434a81255bce800
nicholaspohlmann <pohlmann.nick@gmail.com>
2021-07-03 19:53:31 -0400

    Added FXML and started to work on App

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -6,17 +6,31 @@
 package ucf.assignments;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+//import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App {
+public class App extends Application{
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
+
+            Scene scene = new Scene(root);
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Calculator");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
